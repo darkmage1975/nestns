824781a6c1bb5fe038b2f1f841220442cae0c13c
Stefano Accorsi <stefano.accorsi@inwind.it>
2021-12-13 14:37:30 +0100

    First configs

--- index.js
+++ index.js
@@ -2,11 +2,26 @@ const cool = require('cool-ascii-faces');
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+const {
+  dialogflow,
+  actionssdk,
+  Image,
+  Table,
+  Carousel,
+} = require('actions-on-google');
 
-express()
+// Create an app instance
+const app = actionssdk({debug: true});
+
+/*express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .get('/cool', (req, res) => res.send(cool()))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))*/
+
+app.intent('actions.intent.Welcome', (conv) => {
+  	conv.ask('Ciaone!');
+});
+
