0803eab36e1dca02d965ab2c5a6071829672e394
Stefano Accorsi <stefano.accorsi@inwind.it>
2021-12-13 17:39:56 +0100

    New test

--- index.js
+++ index.js
@@ -2,13 +2,7 @@ const cool = require('cool-ascii-faces');
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
-const {
-  dialogflow,
-  actionssdk,
-  Image,
-  Table,
-  Carousel,
-} = require('actions-on-google');
+const { dialogflow, HtmlResponse } = require('actions-on-google');
 
 // Create an app instance
 const app = dialogflow({debug: true});
