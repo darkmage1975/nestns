1f04e77a8290fef0056c35d5514f81d658ae6acb
Stefano Accorsi <stefano.accorsi@inwind.it>
2021-12-13 18:32:29 +0100

    New test

--- index.js
+++ index.js
@@ -1,15 +1,31 @@
 'use strict';
-const express = require('express')
-const bodyParser = require('body-parser')
-const path = require('path')
-const PORT = process.env.PORT || 5000
-const {dialogflow, HtmlResponse} = require('actions-on-google');
-const app = dialogflow({debug: true});
+const express = require('express');
+const {
+  dialogflow,
+  HtmlResponse,
+} = require('actions-on-google');
+const bodyParser = require('body-parser');
+const port = process.env.PORT || 3000;
+const app = dialogflow({debug:true});
 
-app.intent('Welcome', (conv) => {
-  console.log('welcome');
-  conv.ask('Welcome!');
+app.intent('welcome', (conv) => {
+  if (!conv.surface.capabilities
+      .has('actions.capability.INTERACTIVE_CANVAS')) {
+    conv.close('Sorry, this device does not support Interactive Canvas!');
+    return;
+  }
+  conv.ask('Welcome! Do you want me to change color or pause spinning? ' +
+    'You can also tell me to ask you later.');
+  conv.ask(new HtmlResponse({
+    url: `https://aa2010.herokuapp.com`,
+  }));
 });
 
-express().use(bodyParser.json(), app).listen(PORT);
+const expressApp = express().use(bodyParser.json());
+expressApp.post('/webhook', app);
 
+expressApp.get('/da', (req,res)=>{
+    res.send("Sheeesh");
+});
+
+expressApp.listen(port);
